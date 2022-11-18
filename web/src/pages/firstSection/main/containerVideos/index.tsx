eab690b90f23374f0a183d7223f7b2c0795fea97
ewerton11 <ewertonreisc0@gmail.com>
2022-11-17 23:15:02 -0300

    criando slide videos

--- web/src/pages/firstSection/main/containerVideos/index.tsx
+++ web/src/pages/firstSection/main/containerVideos/index.tsx
@@ -1,29 +1,72 @@
 import { useEffect, useState } from "react"
 import { apiVideos } from "../../../../lib/axios/axios"
-import { ContainerDiv, Divvv, StyleDiv } from "./style"
+import {
+  ArrowLeftStyle,
+  ArrowRightStyle,
+  ContainerDiv,
+  StyleDiv,
+  StyleDivLeft,
+  StyleDivRigth,
+} from "./style"
+import ArrowLeft from "../../../../assets/arrowLeft.svg"
+import ArrowRight from "../../../../assets/arrowRight.svg"
+
+interface SerieA {
+  competition: string
+}
 
 export default function FirstSection() {
   useEffect(() => {
     apiVideos.get("").then((Response) => {
       //alterar a rodada dinamico importando rounds
 
-      const teamClashes = Response.data.response
+      const data = Response.data.response
+      const serieA = data.filter(
+        (ligue: SerieA) => ligue.competition === "BRASIL: Serie A"
+      )
 
-      setClashes(teamClashes)
+      // console.log(serieA.length)
+
+      setVideos(serieA)
     })
   }, [])
 
-  const [teamsAgainst, setClashes] = useState([])
+  const [teamsVideos, setVideos] = useState([])
+
+  const [index, setIndex] = useState(0)
+
+  function swipeLeft() {
+    // indexValue
+
+    setIndex(index + 1)
+  }
+
+  let gamesRounds = teamsVideos[index].videos[0].embed
 
   return (
     <ContainerDiv>
+      <ArrowLeftStyle
+        src={ArrowLeft}
+        alt="seta para esquerda"
+        onClick={swipeLeft}
+      />
+      {/* <StyleDivLeft
+        dangerouslySetInnerHTML={{
+          __html: zero,
+        }}
+      ></StyleDivLeft> */}
       <StyleDiv
         dangerouslySetInnerHTML={{
-          __html: teamsAgainst
-            .filter((item) => item.competition === "BRASIL: Serie A")
-            .map((item) => item.videos[0].embed),
+          __html: gamesRounds,
         }}
       ></StyleDiv>
+      {/* <StyleDivRigth
+        dangerouslySetInnerHTML={{
+          __html: dois,
+        }}
+      ></StyleDivRigth> */}
+
+      <ArrowRightStyle src={ArrowRight} alt="seta para direita" />
     </ContainerDiv>
   )
 }
--- web/src/pages/firstSection/style.ts
+++ web/src/pages/firstSection/style.ts
@@ -3,8 +3,6 @@ import styled from "styled-components"
 export const SectionStyle = styled.section`
   width: 60%;
   height: 60%;
-  background-color: aqua;
-  border: 1px solid red;
   margin-top: 5%;
   position: absolute;
   left: 30%;
