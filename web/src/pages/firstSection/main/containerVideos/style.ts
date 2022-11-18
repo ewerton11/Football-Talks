aee5564733869bbdc57470df981e1e56b8c233fa
ewerton11 <ewertonreisc01@gmail.com>
2022-11-17 23:15:02 -0300

    criando slide videos

--- web/src/pages/firstSection/main/containerVideos/style.ts
+++ web/src/pages/firstSection/main/containerVideos/style.ts
@@ -7,13 +7,54 @@ export const ContainerDiv = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
+  overflow-x: auto;
+`
+
+export const ArrowLeftStyle = styled.img`
+  width: 50px;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+`
+
+export const ArrowRightStyle = styled.img`
+  width: 50px;
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+`
+
+export const StyleDivLeft = styled.div`
+  width: 500px;
+  height: 250px;
+  display: flex;
+  overflow: auto;
+  border: 1px solid black;
+  position: absolute;
+  left: 10%;
 `
 
 export const StyleDiv = styled.div`
+  z-index: 2;
   width: 500px;
   height: 300px;
   display: flex;
+  border: 1px solid black;
   overflow: auto;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+`
+
+export const StyleDivRigth = styled.div`
+  width: 500px;
+  height: 250px;
+  border: 1px solid black;
+  display: flex;
+  position: absolute;
+  right: 10%;
 `
 
 /* ${div} {
